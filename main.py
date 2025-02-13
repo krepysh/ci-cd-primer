c7e13bd08488840d99932e44953c43c36228babe
Anton Topchiy <topchiy.anton@gmail.com>
2025-02-13 17:58:07 +0100

    calculating request time

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
+count = 0
+
 
 @app.route("/")
-def index():
-    return "Hello, World from Germany!"
+def index(count=count):
+    count += 1
+    return f"Hello, World from Germany! You visited this page {count} times."
 
 
 if __name__ == "__main__":
