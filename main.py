9020cb60a80069c2b4bfabbcd9349b911ec1b470
Anton Topchiy <topchiy.anton@gmail.com>
2025-02-13 18:08:30 +0100

    mutable object

--- main.py
+++ main.py
@@ -3,14 +3,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
-count = 0
+count = [0]
 
 
 @app.route("/")
 def index():
-    global count
-    count += 1
-    return f"Hello, World from Germany! You visited this page {count} times."
+    count[0] += 1
+    return f"Hello, World from Germany! You visited this page {count[0]} times."
 
 
 if __name__ == "__main__":
